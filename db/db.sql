d5386c05d1897a7e0f5e41d3d00aaf43bc489790
Tesfa8186 <tesfa1978@gmail.com>
2023-02-27 21:23:07 +0800

    update files

---
+++ db/db.sql
@@ -0,0 +1,8 @@
+-- Drop fitness_db if it exists currently --
+DROP DATABASE IF EXISTS fitness_db;
+
+-- Create the fitness_db database --
+CREATE DATABASE fitness_db;
+
+-- Use fitness_db database --
+USE fitness_db;
