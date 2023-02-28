a022accfa42db589dd2fc4944171317630e0e4ba
Focrult <focrult@gmail.com>
2023-02-28 19:48:49 +0800

    Inserted code into seeds, utils and models

---
+++ seeds/user.js
@@ -0,0 +1,41 @@
+const { User } = require('../models');
+
+const userInfo = [
+    {
+    "user_id": 1,
+      "username": "Elon Musk",
+      "email": "example@gmail.com",
+      "password": "password123"
+    },
+    {
+        "user_id": 2,
+      "username": "",
+      "email": "mvp@gmail.com",
+      "password": "ilikeice8002"
+    },
+    {
+        "user_id": 3,
+      "username": "Uther",
+      "email": "lightbringer@storm.com",
+      "password": "dontdisthelight2002"
+    },
+    {
+        "user_id": 4,
+      "username": "mega",
+      "email": "mind@metro.com",
+      "password": "Presentation!"
+    },
+    {
+        "user_id": 5,
+      "username": "Rosh",
+      "email": "WOD@okay.com",
+      "password": "WOD>BFA+SL"
+    }
+  ];
+
+  const seedUsers = () => User.bulkCreate(userInfo);
+  module.exports = seedUsers;
+
+
+
+
