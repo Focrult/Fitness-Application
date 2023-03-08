db746676848ac342176a00fdb2c9590c6ca553d9
unknown <zhou7cathy@gmail.com>
2023-03-08 12:27:00 +0800

    merged data in accont.js into user.js.

--- seeds/user.js
+++ seeds/user.js
@@ -3,28 +3,39 @@ const { User } = require('../models');
 const userInfo = [
   {
     "username": "Elon Musk",
-    "email": "example@gmail.com",
-    "password": "password123"
+    "email": "elon@gmail.com",
+    "password": "password123",
+    "gender": "male",
+    "height": 185,
+    "weight": 110,
+    // "preference": "swimming, running, weight lifting"
   },
   {
-    "username": "mvp",
-    "email": "mvp@gmail.com",
-    "password": "ilikeice8002"
+    "username": "Luke Bake",
+    "email": "luke@gmail.com",
+    "password": "ilikeice8002",
+    "gender": "male",
+    "height": 178,
+    "weight": 100,
+    // "preference": "swimming, running, weight lifting"
   },
   {
-    "username": "Uther",
-    "email": "lightbringer@storm.com",
-    "password": "dontdisthelight2002"
+    "username": "Rachel Barbar",
+    "email": "rachel@storm.com",
+    "password": "dontdisthelight2002",
+    "gender": "female",
+    "height": 160,
+    "weight": 70,
+    // "preference": "swimming, running, weight lifting"
   },
   {
-    "username": "mega",
-    "email": "mind@metro.com",
-    "password": "Presentation!"
-  },
-  {
-    "username": "Rosh",
-    "email": "WOD@okay.com",
-    "password": "WOD>BFA+SL"
+    "username": "Mega Rosh",
+    "email": "mega@metro.com",
+    "password": "Presentation!",
+    "gender": "female",
+    "height": 165,
+    "weight": 60,
+    // "preference": "swimming, running, weight lifting"
   }
 ];
 
