1845b8adde0be374f8e33cacfbf5bf72b482af79
unknown <zhou7cathy@gmail.com>
2023-03-11 13:40:26 +0800

    updated question route, working good.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user_routes');
 const chartRoute = require('./graph_routes');
 const workoutRoute = require('./workout_routes');
+const questionRoute = require('./question_routes');
 
 router.use('/user', userRoutes);
 router.use('/matrix', chartRoute);
 router.use('/workout', workoutRoute);
-
+router.use('/form', questionRoute);
 
 module.exports = router;
--- public/js/question.js
+++ public/js/question.js
@@ -1,30 +1,27 @@
-const fitnessQuestions = [
-    {
-      question: "What is your current weight in kilograms?",
-      type: "number",
-      name: "weight",
-      placeholder: "Enter your weight in kilograms",
-      required: true
-    },
-    {
-      question: "What is your current height in centimeters?",
-      type: "number",
-      name: "height",
-      placeholder: "Enter your height in centimeters",
-      required: true
-    },
-    {
-      question: "What is your gender?",
-      type: "radio",
-      name: "gender",
-      options: [
-        { value: "male", label: "Male" },
-        { value: "female", label: "Female" },
-        { value: "other", label: "Other" }
-      ],
-      required: true
-    }
-  ];
-  
-  module.exports = fitnessQuestions;
-  
+// const fitnessQuestions = [
+//     {
+//       question: "What is your current weight in kilograms?",
+//       type: "number",
+//       name: "weight",
+//       placeholder: "Enter your weight in kilograms",
+//       required: true
+//     },
+//     {
+//       question: "What is your current height in centimeters?",
+//       type: "number",
+//       name: "height",
+//       placeholder: "Enter your height in centimeters",
+//       required: true
+//     },
+//     {
+//       question: "What is your gender?",
+//       type: "radio",
+//       name: "gender",
+//       options: [
+//         { value: "male", label: "Male" },
+//         { value: "female", label: "Female" },
+//         { value: "other", label: "Other" }
+//       ],
+//       required: true
+//     }
+//   ];
