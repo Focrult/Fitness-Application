29208b72364b4d5c6f094ba941c3fa7aafd8ef7d
Tesfa8186 <tesfa1978@gmail.com>
2023-03-13 18:22:00 +0800

    Update readme

--- models/index.js
+++ models/index.js
@@ -1,26 +1,25 @@
-const User = require("./user");
-const Workout = require("./workout");
-const Exercise = require("./exercise");
-const WorkoutExercise = require("./workout_exercise");
+const User = require("./User");
+const Workout = require("./Workout");
+const Exercise = require("./Exercise");
+const WorkoutExercise = require("./Workout_exercise");
 
 // Define Associations
 Workout.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 User.hasMany(Workout, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Exercise.belongsToMany(Workout, {
-  through: {model: WorkoutExercise},
+  through: { model: WorkoutExercise },
 });
 
 Workout.belongsToMany(Exercise, {
-  through: {model: WorkoutExercise},
+  through: { model: WorkoutExercise },
 });
-
 
 module.exports = {
   User,
